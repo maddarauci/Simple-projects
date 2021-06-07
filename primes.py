686225bad2db0e8e94246e9e5e19eb7487cd2b22
maddarauci <54892387+maddarauci@users.noreply.github.com>
2021-06-08 09:00:05 +1000

    Add files via upload
    tells you whether the provided number is prime or not

---
+++ primes.py
@@ -0,0 +1,17 @@
+# prime numbers
+
+num = int(input('enter a number: '))
+
+flag = False
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+if flag:
+    print(num, ' is not a prime')
+
+else:
+    print(num, 'is a prime number')
