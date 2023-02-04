e900a79cbf36a1c2034d880e3b6b35b182f175ef
maddarauci <dominicd165@gmail.com>
2023-02-05 01:55:39 +1100

    new file:   BM_string_search.c

---
+++ BM_string_search.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ALPHABET_LEN 256
+#define max(a, b) ((a < b) ? b : a)
+
+// BAD CHARACTER RULE.
+//
+
+
