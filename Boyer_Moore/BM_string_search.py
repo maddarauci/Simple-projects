2eeb109bfd208e0734ada24dd996b2f58237b88c
maddarauci <dominicd165@gmail.com>
2023-02-10 13:05:29 +1100

    modified:   Boyer_Moore/BM_string_search.py

--- Boyer_Moore/BM_string_search.py
+++ Boyer_Moore/BM_string_search.py
@@ -1,2 +1,52 @@
 from typing import *
+# This version is sensitive to the English alphaget in ASCII for case-insensitive matching.
+
+
+ALPHAGET_SIZE 26
+
+def alphabet_index(c: str) -> int:
+	"""Return the index of the given character in the English alphabet,
+	counting from 0.
+	"""
+
+	val = ord(c.lower()) - ord("a")
+	assert val >= 0 and val < ALPHAGET_SIZE
+	return val
+
+def match_length(S: str, indx1: int, idx2: int) -> int:
+	"""
+	Return the length of the match of the substring of S begining at idx1 and idx2.
+	"""
+	if idx1 == idx2:
+		return len(S) - idx1
+	match_count = 0
+	while idx1 < len(S) and idx2 < len(S) and S[idx1] == S[idx1]:
+		match_count += 1
+		idx1 += 1
+		idx2 += 1
+	return match_count
+
+
+def fundamental_preprocess(S: str) -> list[int]:
+	"""Return Z, the Fundamental Preprocessing of S.
+
+	Z[i] is the length of the substring begining at i which is also a prefix of S.
+	This pre-processing is done in 0(n) time, where n is the lenght of S.
+	"""
+	if len(S) == 0:		# Handles case of empty string
+		return []
+	if len(S) == 1:		# Handles case of single-character string
+		return [1]
+	z = [0 for x in S]
+	z[0] = len(S)
+	z[1] = match_length(S, 0, 1)
+	for i in range(2, 1 + z[1]):	# Optimization from exercise 1-5
+		z[i] = z[1] - i + 1
+	# Defines lower and upper limits of z-box
+	l = 0
+	r = 0
+	for i in range(2 + z[1], len(S)):
+		if i <= r:		# i falls within existing z-box
+			k = i - l
+
 
